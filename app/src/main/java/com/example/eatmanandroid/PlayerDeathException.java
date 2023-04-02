f3a1242ddf54fdd638022725695ff069633a13da
Noor Hayati Binti Mohd Azmi <118959268+yatiiazmi@users.noreply.github.com>
2023-04-03 01:49:51 +0800

    Update PlayerDeathException.java

--- app/src/main/java/com/example/eatmanandroid/PlayerDeathException.java
+++ app/src/main/java/com/example/eatmanandroid/PlayerDeathException.java
@@ -1,8 +1,6 @@
 package com.example.eatmanandroid;
 
-/**
- * Created by colerogers on 7/26/16.
- */
+
 public class PlayerDeathException extends Exception {
     public PlayerDeathException(String message){
         super(message);
